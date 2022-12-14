e2278b56f40a1b2e66d61893fe3e2128eefe988e
mzaouchi <mzaouchi@gmail.com>
2022-12-14 12:07:41 +0100

    first commit

---
+++ wsjs.js
@@ -0,0 +1,130 @@
+// console.log('Hello Javascript')
+
+// Simple
+// var str = "Mamino2003"
+// console.log(str)
+// console.log(typeof(str))
+
+// var nbr = 23.7
+// console.log(typeof(nbr))
+
+// var bool = true
+// console.log(typeof(bool))
+
+// var un
+// console.log(typeof(un))
+
+// var testN = null
+// console.log(typeof(testN))
+
+// Complex
+// var arr = [1,2,3,4,4]
+
+// console.log(arr[2])
+// console.log(arr.length)
+// console.log(typeof(arr))
+
+// var user = {name : "Amine",old : 19,track : "FullStackJS"}
+// // console.log(user)
+// // console.log(user.name)
+// // console.log(user.track)
+// console.log(Object.entries(user))
+// console.log(Object.keys(user))
+// console.log(Object.values(user))
+
+// var a = 5
+// var b = 2
+
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+// a++
+// a++
+// console.log(a)
+// a--
+// console.log(a)
+// console.log(a**b)
+// console.log(++a)
+
+// var a = 5
+// var b = 7
+// var c = "5"
+
+// console.log(a == b)
+// console.log(a == c)
+// console.log(a === c)
+
+// console.log(a!= c)
+// console.log(a !== c)
+
+// console.log(a>b && a<b)
+// console.log(a > b || a < b)
+
+// var age = 18
+
+// if(age<18){
+//   console.log('Enfant')
+// }else if(age<60){
+//   console.log('Adule')
+// }else{
+//   console.log('Vieux')
+// }
+
+// var nbr = 2
+
+// if(nbr % 2 == 0){
+//   console.log('Paire')
+// }else{
+//   console.log('Impair')
+// }
+
+
+// var a = 77
+
+// switch(a){
+//   case 3 : console.log('Trois')
+//            break
+//   case 5 : console.log('Cinq')
+//            break
+//   default : console.log('Amine')
+//   }
+
+
+//   function Somme(a,b){
+//     return a+b
+//   }
+  
+  
+//   console.log(Somme(2,3))
+//   console.log(Somme(8,7))
+
+// var tab = [1,2,4,5,9]
+
+// for(var i = 0; i<tab.length;i++){
+//   console.log(tab[i])
+// }
+
+// for(var key in tab){
+//   console.log(tab[key])
+// }
+
+// for(var mamino of tab){
+//   console.log(mamino)
+// }
+
+
+// var tab = [1,2,4,5,9]
+
+// var i = 9
+
+// do {
+//     console.log(tab[i])
+//     i++
+// } while (i<tab.length);
+
+// while (i<tab.length) {
+//     console.log(tab[i])
+//     i++
+// }
